127dfa00716ab0ded12516f479e632a36269bf34
Emmanueldabs <100179572+Emmanueldabs@users.noreply.github.com>
2022-04-12 20:39:25 -0700

    Add files via upload

---
+++ c35_age calculator .js
@@ -0,0 +1,24 @@
+function calculate(){
+    var datevalue=document.getElementById("Date").value;
+    var monthvalue=document.getElementById("Month").value;
+    var yearvalue=document.getElementById("Year").value;
+
+    var date=new Date();
+
+    var d=date.getDate();
+    var m=1+date.getMonth();
+    var y=date.getFullYear();
+    var month=[31,28,31,30,31,30,31,31,30,31,30,31];
+    if(datevalue>d){
+        d=d+month[m-1];
+        m=m-1;
+    }
+    if(monthvalue>m){
+        m=m+12;
+        y=y-1;
+    }
+    var dd=d-datevalue;
+    var mm=m-monthvalue;
+    var yy=y-yearvalue;
+    document.getElementById("result").innerHTML="Your age is "+yy+" years "+mm+" months "+dd+" days ";
+}
